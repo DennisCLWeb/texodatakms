5b5d3b90d1589e4737a876f3a45d1a60a2bf7799
DennisCLWeb <dennis@clweb.nl>
2026-02-03 17:23:59 +0100

    faq code beter gemaakt, about us pagina gemaakt met contact form, validatie, responsive

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -4,5 +4,15 @@ return [
 
     'unique_entry_value' => 'The :attribute has already been taken.',
     'unique_user_value' => 'The :attribute has already been taken.',
+    'required' => ':attribute is verplicht.',
+    'alpha' => ':attribute mag alleen letters bevatten.',
+    'email' => ':attribute is ongeldig.',
 
+    'attributes' => [
+        'name' => 'Naam',
+        'email' => 'Email',
+        'reason' => 'Reden',
+        'message' => 'Bericht',
+    ],
 ];
+
